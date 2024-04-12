9342aafab1708ef4362b18b5adc1a6c0afd8d221
hajihajhaj <000915724@mohawkcollege.ca>
2024-04-12 14:16:29 -0500

    Add ball movement
    -I added the ball movement and made it reset back to the middle when it goes out of bounds.
    -this is now a working game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public Rigidbody2D ball;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitializeBall();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public void  InitializeBall()
+   {
+       float angle = Random.Range(0f, 30f);
+       float r = Random.Range(0f, 1f);
+       if (r < 0.25f)
+            angle = 180f - angle;
+       else if (r < 0.5f)
+            angle += 180f;
+       else if (r < 0.75f)
+            angle = 360f - angle;
+       angle *= Mathf.Deg2Rad;
+
+       Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+       ball.velocity = dir.normalized * 10f;
+
+       ball.transform.position = Vector3.zero;
+   }
 }
